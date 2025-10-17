3cd72fbe790385340a8e3ce1cf4078b2ba145fde
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-17 15:16:50 -0500

    refactored code, made redundanat items on homepage into a component called InfoCard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import homeIcon1 from '../assets/homeIcon1.svg';
 import item1 from '../assets/item1.svg';
 import item2 from '../assets/item2.svg';
 import item3 from '../assets/item3.svg';
+import InfoCard from '../components/InfoCard'
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
@@ -32,21 +33,24 @@ const Home = () => {
             Get affordable pricing and quick delivery times. 
           </div>
           <div className="items">
-            <div className="item">
-              <img className="imgItem1" src={item1} alt="illustration of man moving package" loading="lazy"/>
-              <b>Quick, easy shipping</b>
-              <p> Send packages without the hassle. Fast, simple, and dependable from start to finish. </p>
-            </div>
-            <div className="item"> 
-              <img className="imgItem2" src={item2} alt="illustration of woman managing packages" loading="lazy"/>
-              <b>Your packages, our priority</b>
-              <p>We treat every package like it’s our own. Secure, efficient, and always on schedule.</p>
-            </div>
-            <div className="item">
-              <img className="imgItem3" src={item3} alt="illustration of man carrying package" loading="lazy"/>
-              <b>Personal and reliable</b>
-              <p>Reliable shipping backed by real people who care about your packages and your experience.</p>
-            </div>
+            <InfoCard 
+              img={item1} 
+              imgDescription="illustration of man moving package" 
+              title="Quick, easy Shipping" 
+              paragraph="Send packages without the hassle. Fast, simple, and dependable from start to finish."
+            />
+            <InfoCard
+              img={item2}
+              imgDescription="illustration of woman managing packages" 
+              title="Your packages, our priority"
+              paragraph="We treat every package like it’s our own. Secure, efficient, and always on schedule."
+            />
+            <InfoCard
+              img={item3}
+              imgDescription="illustration of man carrying package" 
+              title="Personal and reliable"
+              paragraph="Reliable shipping backed by real people who care about your packages and your experience."
+            />
           </div>
       </div>
       <div className="section3">
