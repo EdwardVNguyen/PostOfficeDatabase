9b2b5708aed4a17ae5cd97f48ed003560f0bc185
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-18 11:48:30 -0500

    added sign up with password, need to work on inputs for address

--- frontend/src/components/AuthInput.jsx
+++ frontend/src/components/AuthInput.jsx
@@ -3,7 +3,7 @@ import './AuthInput.css';
 const AuthInput = (props) => {
   return (
     <div className="authInput">
-      <input type={props.type} id={props.id} className="form_input" autoComplete="off" placeholder=" "/>
+      <input type={props.type} id={props.id} className="form_input" autoComplete="off" placeholder=" " onChange={props.onChange} value={props.value}/>
       <label htmlFor={props.htmlFor} className="form_label">{props.text}</label>
     </div>
   );
--- frontend/src/pages/LogInOrSignUp.jsx
+++ frontend/src/pages/LogInOrSignUp.jsx
@@ -6,23 +6,45 @@ import { useState } from 'react';
 
 const LogInOrSignUp = () => {
   const [mode, switchMode] = useState("Login"); 
+  const [phoneValue, setPhoneValue] = useState("");
+
+  const handlePhone = (e) => {
+    let input = e.target.value.replace(/\D/g, ""); // remove non-digits
+    if (input.length > 3 && input.length <= 6)
+      input = `${input.slice(0, 3)}-${input.slice(3)}`;
+    else if (input.length > 6)
+      input = `${input.slice(0, 3)}-${input.slice(3, 6)}-${input.slice(6, 10)}`;
+    setPhoneValue(input);
+  };
 
   return (
     <div className="signContainer">
       <div className="left"> 
         <b className="bigText"> Start shipping with confidence today</b>
         <p className="description">SnailMail, a blazing fast delivery service</p>
-
-        <div className="signIn">
-          <AuthInput type="email" id="email-1" htmlFor="email" text="Email"/>
-          <AuthInput type="password" id="password-1" htmlFor="password" text="Password"/>
-          <p className="noAccount"> Don't have an account, 
-    {/* TODO */} <span className={mode==="Login"}>click here </span> 
-            to sign up! 
-          </p>
-          <AuthButton text="Log In"/> 
-        </div>
-
+        
+        {mode === "Login" ? (
+          <div className="signIn">
+            <AuthInput type="email" id="email-1" htmlFor="email" text="Email"/>
+            <AuthInput type="password" id="password-1" htmlFor="password" text="Password"/>
+            <p className="switch"> Don't have an account,  
+              <span className="click" onClick={()=>{switchMode("Signup")}}> click here </span> 
+              to sign up! 
+            </p>
+            <AuthButton text="Log In"/> 
+          </div>
+        ) : (
+          <div className="signUp">
+            <AuthInput type="email" id="email-2" htmlFor="email" text="Email" />
+            <AuthInput type="password" id="password-2" htmlFor="password" text="Password"/>
+            <AuthInput type="tel" id="phone-1" htmlFor="phone" text="Phone Number" onChange={handlePhone} value={phoneValue}/>
+            <p className="switch"> Already have an account,
+              <span className="click" onClick={()=>{switchMode("Login")}}> click here </span>
+              to log in!
+            </p>
+           <AuthButton text="Sign Up" /> 
+          </div>
+        )}
       </div>
       <div className="right">
         <img className="signUpImg" src={signUpImg} alt="image of Company Logo and man carrying packages"/>
