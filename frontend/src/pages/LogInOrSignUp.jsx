001f0ace1fea78e4346825e97a7f5934a00be1ba
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-18 06:09:53 -0500

    the spacing between button and 'don't have account' text was bothering, so I slightly increased the button position

--- frontend/src/pages/LogInOrSignUp.jsx
+++ frontend/src/pages/LogInOrSignUp.jsx
@@ -1,19 +1,28 @@
+import './LogInOrSignUp.css';
 import signUpImg from '../assets/signupImg.svg';
 import AuthInput from '../components/AuthInput';
 import AuthButton from '../components/AuthButton';
-import './LogInOrSignUp.css';
+import { useState } from 'react';
 
 const LogInOrSignUp = () => {
+  const [mode, switchMode] = useState("Login"); 
+
   return (
     <div className="signContainer">
       <div className="left"> 
         <b className="bigText"> Start shipping with confidence today</b>
         <p className="description">SnailMail, a blazing fast delivery service</p>
-        <div className="subContainer">
+
+        <div className="signIn">
           <AuthInput type="email" id="email-1" htmlFor="email" text="Email"/>
           <AuthInput type="password" id="password-1" htmlFor="password" text="Password"/>
+          <p className="noAccount"> Don't have an account, 
+    {/* TODO */} <span className={mode==="Login"}>click here </span> 
+            to sign up! 
+          </p>
           <AuthButton text="Log In"/> 
         </div>
+
       </div>
       <div className="right">
         <img className="signUpImg" src={signUpImg} alt="image of Company Logo and man carrying packages"/>
