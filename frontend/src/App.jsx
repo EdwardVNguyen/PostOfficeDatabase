7eb46a40f9dc0d38066d1a2243caa13d89270dc4
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-18 01:39:59 -0500

    lazy load pages that will not be used often

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes , Route } from 'react-router-dom';
+import { Suspense, lazy } from "react";
 
 import Home from './pages/Home';
-import Shipping from './pages/Shipping';
-import Tracking from './pages/Tracking';
-import About from './pages/About';
-import Support from './pages/Support';
 import LoginOrSignUp from './pages/LogInOrSignUp';
+
+const Shipping = lazy( () => import('./pages/Shipping'));
+const Tracking = lazy( () => import('./pages/Tracking'));
+const About = lazy( () => import('./pages/About'));
+const Support = lazy( () => import('./pages/Support'));
 
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -17,6 +19,7 @@ const App = () => {
       <NavBar />
       
       <main>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shipping" element={<Shipping/>} />
@@ -25,6 +28,7 @@ const App = () => {
           <Route path="/support" element={<Support/>} />
           <Route path="/loginorsignup" element={<LoginOrSignUp/>} />
         </Routes>
+      </Suspense>
       </main>
 
       <Footer />
