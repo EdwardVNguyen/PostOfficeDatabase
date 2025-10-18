f9d10c2a5c4d32d0c32f8f25234f100c17d47183
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-18 12:42:02 -0500

    now able to store values from inputs for user authentication

--- frontend/src/components/AuthInput.jsx
+++ frontend/src/components/AuthInput.jsx
@@ -3,7 +3,15 @@ import './AuthInput.css';
 const AuthInput = (props) => {
   return (
     <div className="authInput">
-      <input type={props.type} id={props.id} className="form_input" autoComplete="off" placeholder=" " onChange={props.onChange} value={props.value}/>
+      <input name={props.name} 
+             type={props.type} 
+             id={props.id} 
+             className="form_input" 
+             autoComplete="off" 
+             placeholder=" " 
+             onChange={props.onChange} 
+             value={props.value ?? ""} // makes it so where prop value is never undefined
+      />
       <label htmlFor={props.htmlFor} className="form_label">{props.text}</label>
     </div>
   );
--- frontend/src/pages/LogInOrSignUp.jsx
+++ frontend/src/pages/LogInOrSignUp.jsx
@@ -8,6 +8,18 @@ const LogInOrSignUp = () => {
   const [mode, switchMode] = useState("Login"); 
   const [phoneValue, setPhoneValue] = useState("");
 
+  // gets input from AuthInput component
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData( (prev) => ({ ...prev, [name]: value}));
+  }
+
+  // removes non digits and formats it like so: 123-456-7890
   const handlePhone = (e) => {
     let input = e.target.value.replace(/\D/g, ""); // remove non-digits
     if (input.length > 3 && input.length <= 6)
@@ -25,8 +37,8 @@ const LogInOrSignUp = () => {
         
         {mode === "Login" ? (
           <div className="signIn">
-            <AuthInput type="email" id="email-1" htmlFor="email" text="Email"/>
-            <AuthInput type="password" id="password-1" htmlFor="password" text="Password"/>
+            <AuthInput name="email" type="email" id="email-1" htmlFor="email" text="Email" onChange={handleChange} value={formData.email}/>
+            <AuthInput name="password" type="password" id="password-1" htmlFor="password" text="Password" onChange={handleChange} value={formData.password}/>
             <p className="switch"> Don't have an account,  
               <span className="click" onClick={()=>{switchMode("Signup")}}> click here </span> 
               to sign up! 
@@ -35,8 +47,8 @@ const LogInOrSignUp = () => {
           </div>
         ) : (
           <div className="signUp">
-            <AuthInput type="email" id="email-2" htmlFor="email" text="Email" />
-            <AuthInput type="password" id="password-2" htmlFor="password" text="Password"/>
+            <AuthInput name="email" type="email" id="email-2" htmlFor="email" text="Email" onChange={handleChange} value={formData.email}/>
+            <AuthInput name="password" type="password" id="password-2" htmlFor="password" text="Password" onChange={handleChange} value={formData.password}/>
             <AuthInput type="tel" id="phone-1" htmlFor="phone" text="Phone Number" onChange={handlePhone} value={phoneValue}/>
             <p className="switch"> Already have an account,
               <span className="click" onClick={()=>{switchMode("Login")}}> click here </span>
