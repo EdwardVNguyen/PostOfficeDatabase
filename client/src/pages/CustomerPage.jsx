a422be3644f2b6f16077f6b1d59b50d3412f045a
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-25 22:12:52 -0500

    starting customer page

--- client/src/pages/CustomerPage.jsx
+++ client/src/pages/CustomerPage.jsx
@@ -1,6 +1,32 @@
+import './CustomerPage.css';
+
 const CustomerPage = () => {
   return (
-    <div> Customer Page </div>
+    <div className="customerPageContainer">
+      <div className="subContainer1">
+        <div className="profile">
+          <div className="profileLeft"> 
+            <em className="icon"> Profile Icon </em>
+            <em className="accountID"> Account ID</em>
+            <div className="viewProfile"> View my profile</div>
+          </div>
+          <div className="postOffice"> Nearest post office </div>
+        </div>
+        <div className="tracker">
+          <b> Tracking ID </b>
+          <form className="customerPageTracker">
+            <input />
+            <button> Track </button>
+          </form>
+        </div> 
+      </div>
+      <div className="mostRecentPackage">
+        Recent Packages
+      </div>
+      <div className="quickLinks">
+        Quick Links
+      </div>
+    </div>
   );
 };
 
