f65dc36f7e2ef3364ddf3fa39411b006bf3ff95e
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-26 23:48:24 -0500

    added pagination funactionality to user shipping, made ag-grid for customer shipments, added buttons but not implemented functionality for those yet

--- client/src/pages/UserShipping.jsx
+++ client/src/pages/UserShipping.jsx
@@ -5,6 +5,8 @@ import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+import Pagination from '../utils/Pagination';
+
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 const UserShipping = ( {globalAuthId }) => {
@@ -13,24 +15,24 @@ const UserShipping = ( {globalAuthId }) => {
     const [totalPages, setTotalPages] = useState(1);
     const [loading, setLoading] = useState(false);
 
-    const limit = 15; // number of packages per request
+    const limit = 12; // number of packages per request
     const authId = globalAuthId;
 
     const fetchPackages = async (pageNum = 1) => {
       setLoading(true);
       try {
         const res = await fetch( `${import.meta.env.VITE_API_URL}/getCustomerPackageData?authId=${authId}&page=${pageNum}&limit=${limit}`);
-        const data = await res.json();
+        const packageData = await res.json();
 
-        console.log(data);
+        console.log(packageData);
 
-        if (data.success) {
+        if (packageData.success) {
           // Append new packages if not the first page
-          setPackages(data.packages);
-          setTotalPages(data.totalPages);
+          setPackages(packageData.packages);
+          setTotalPages(packageData.totalPages);
           setPage(pageNum);
         } else {
-          console.error("Failed to fetch packages", data.message);
+          console.error("Failed to fetch packages", packageData.message);
         }
       } catch (err) {
         console.error("Error fetching packages:", err);
@@ -54,24 +56,27 @@ const UserShipping = ( {globalAuthId }) => {
       Weight: pkg.weight,
       Dimensions: `${Math.round(pkg.length)}x${Math.round(pkg.width)}x${Math.round(pkg.height)}`,
       PackageStatus: pkg.package_status,
-      DateCreated: pkg.created_at,
-      LastModified: pkg.last_updated
+      DateCreated: new Date(pkg.created_at).toISOString().split('T')[0],
+      LastModified: new Date(pkg.last_updated).toISOString().split('T')[0]
     }));
         
   // Column Definitions: Defines the columns to be displayed.
     const [colDefs, setColDefs] = useState([
         { 
           field: "ID",
-          flex: 0.5
+          flex: 0.4
         },
         { 
           field: "Type",
-          flex: 0.75
+          flex: 0.70
         },
-        { field: "PackageStatus"},
+        { 
+          field: "PackageStatus",
+          flex: 0.85
+        },
         { 
           field: "Recipient",
-          flex: 1.25
+          flex: 0.85
         },
         { 
           field: "DropoffAddress",
@@ -79,46 +84,56 @@ const UserShipping = ( {globalAuthId }) => {
         },
         { 
           field: "Dimensions",
-          headerName: "LxWxH (in cm)"
+          headerName: "LxWxH (in cm)",
+          flex: 0.85
         },
         { 
           field: "Weight",
           headerName: "Weight (in kg)",
-          flex: 0.85,
+          flex: 0.75,
         },
         { 
           field: "LastModified",
-          flex: 0.85
+          flex: 0.75,
+          sort: "desc" // makes the latest modfication date go on top
         },
         { 
           field: "DateCreated",
-          flex: 0.85
+          flex: 0.75
         }
     ]);
 
   return (
     <div className="userShippingContainer">
-      <b> Your Shipments, Anytime Anywhere </b>
-      <div className="ag-theme-alpine" style={{ height: 500,width: 1350}}>
-          <AgGridReact
-              rowData={rowData}
-              columnDefs={colDefs}
-          />
+      <div className="userShippingSideBar"> 
+        <div/>
+        <button className="userShippingBtn"> Your Shipments </button>
+        <button className="userShippingBtn"> Create Shipment </button>
+        <button className="userShippingBtn"> E-Commerce </button>
+        <button className="userShippingBtn"> Settings </button>
+        <button className="userShippingBtn"> Help </button>
       </div>
-      <div className="pagination">
-        {Array.from({ length: totalPages }, (_, i) => (
-          <button 
-            key={i+1} 
-            onClick={() => fetchPackages(i+1)}
-            style={{ 
-              margin: '0 5px', 
-              fontWeight: page === i+1 ? 'bold' : 'normal'
-            }}
-          >
-            {i+1}
-          </button>
-        ))}
-      </div>
+      <div className="userShippingRight">
+        <div className="userShippingTop">
+          <div className="userShippingDesc">
+            SnailMail <span> Your shipments, anywhere, anytime</span>
+          </div>
+            <Pagination
+                  currentPage={page}
+                  totalCount={totalPages * limit} // total items
+                  pageSize={limit}
+                  onPageChange={fetchPackages}
+             />
+          </div>
+        <div className="ag-theme-alpine" style={{ height: 550, width: "100%" }}>
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={colDefs}
+                pagination={false}
+            />
+        </div>
+    </div>
+      
     </div>
   );
 };
