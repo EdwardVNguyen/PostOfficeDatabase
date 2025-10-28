4a5a32f3746d0babb929a1e99ab94f87bc3a3f52
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-28 16:43:46 -0500

    trying to get railway to work 2

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 });
 
